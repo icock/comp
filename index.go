5b823d31119b71c008d5294dc4cbd2518c6aaf20
Eli Cox <mini@cock.li>
2017-07-22 02:13:10 +0000

    index: markup

--- index.go
+++ index.go
@@ -6,11 +6,11 @@ Send any feedback (comments, typos, issues, questions) to
 
 https://github.com/icock/comp/issues
 
-Or open a pull requests at
+or open a pull requests at
 
 https://github.com/icock/comp/pulls
 
-Or send a email to me directly.
+or send a email to me directly.
 My email address is listed on my personal homepage:
 
 https://icock.github.io
