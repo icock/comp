0374a2d564d9829bd8f01522c451d4fa528e9616
Eli Cox <mini@cock.li>
2017-07-22 02:05:25 +0000

    Add contact and license info.

--- index.go
+++ index.go
@@ -1,2 +1,22 @@
-/* I abuse godoc to take notes on computers, programming etc. */
+/* I abuse godoc to take notes on computers, programming etc.
+
+Contact
+
+Send any feedback (comments, typos, issues, questions) to
+
+https://github.com/icock/comp/issues
+
+Or open a pull requests at
+
+https://github.com/icock/comp/pulls
+
+Or send a email to me directly.
+My email address is listed on my personal homepage:
+
+https://icock.github.io
+
+License
+
+public domain
+*/
 package comp
